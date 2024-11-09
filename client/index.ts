d912958701e5780676b89722be78d0c4a4b307ab
rafaelmasselli <rafaelmasselli0@gmail.com>
2024-11-09 18:02:40 -0300

    fix: generate connection with the bot

--- client/index.ts
+++ client/index.ts
@@ -3,22 +3,18 @@ import { handleBot } from "./src/bot";
 
 import { Client } from "whatsapp-web.js";
 
-try {
-  const client = new Client({ webVersionCache: { type: "none" } });
+const client = new Client({});
 
-  client.on("qr", (qr) => {
-    qrcode.generate(qr, { small: true });
-  });
+client.on("qr", (qr) => {
+  qrcode.generate(qr, { small: true });
+});
 
-  client.on("ready", () => {
-    console.log("The bot is connected and ready for use!");
-  });
+client.on("ready", () => {
+  console.log("The bot is connected and ready for use!");
+});
 
-  client.on("message", async (message) => {
-    await handleBot(message);
-  });
+client.on("message", async (message) => {
+  await handleBot(message);
+});
 
-  client.initialize();
-} catch (error) {
-  console.log(error);
-}
+client.initialize();
